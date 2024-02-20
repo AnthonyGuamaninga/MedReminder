3534c245126fecffab4fa296f90c695dbb3f7a0b
mosho1998 <bryan_fernando96@hotmail.com>
2024-02-19 19:22:00 -0500

    vistas faltantes

---
+++ app/src/main/java/com/grupo4/recordatoriosmedicamentos/ui/activities/VistaPrincipalActivity.kt
@@ -0,0 +1,12 @@
+package com.grupo4.recordatoriosmedicamentos.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupo4.recordatoriosmedicamentos.R
+
+class VistaPrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vistaprincipal)
+    }
+}
